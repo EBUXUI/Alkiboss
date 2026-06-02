ef5361adf10cbaa263d657c16d9e44525f68c769
EBUXUI <123589822+EBUXUI@users.noreply.github.com>
2026-06-02 21:31:44 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,10 @@ export default function Home() {
                 <Stats/>
                 {/*<MusicCatalog/>*/}
                 <DisabilityHero/>
-                <HeroComponentII/>
+                <div id="BrandPartnerships">
+                    <HeroComponentII/>
+                </div>
+                
                 <MainHero />
                  <Menu />
             </PageFormat>
