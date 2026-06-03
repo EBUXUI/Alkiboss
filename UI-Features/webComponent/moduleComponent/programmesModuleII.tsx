d0247ee6d4a41c30424ab9467fbea7cd2a68a57c
EBUXUI <123589822+EBUXUI@users.noreply.github.com>
2026-06-03 10:20:54 +0300

    Update programmesModuleII.tsx

--- UI-Features/webComponent/moduleComponent/programmesModuleII.tsx
+++ UI-Features/webComponent/moduleComponent/programmesModuleII.tsx
@@ -14,32 +14,32 @@ export default function programmesModule() {
   const [oneWay, setOneWay] = useState(false);
 
   const videoData = [
-    {
-      video: "/Videos/VideoI.mp4",
-      title: "Grace Funding Platform",
-      link: "/FoundationEntry/CrowdFunding",
-      preview: "/FoundationEntry/CrowdFunding",
-    },
-    {
-      video: "/Videos/VideoJ.mp4",
-      title: "Grace Virtual Platform",
-      link: "/GlobalParticipation/Volunteering/VirtualVolunteering",
-      preview: "/GlobalParticipation/Volunteering/VirtualVolunteering",
-    },
-    {
-      video: "/Videos/VideoK.mp4",
-      title: "Grace Community Seminar",
-      link: "/GlobalParticipation/Awareness/CommunitySeminar",
-      preview: "/GlobalParticipation/Awareness/CommunitySeminar",
-    },
-    {
-      video: "/Videos/VideoC.mp4",
-      title: "Grace Medical Training",
-      link: "/LaunchingSoon",
-      preview: "/LaunchingSoon",
-    },
+        {
+            video: "/Videos/VideoK.mp4",
+            title: "Afro Fusion Storytelling",
+            link: "https://www.tiktok.com/@alkiboss_classic/video/7637590092445404423",
+            preview: "https://www.tiktok.com/@alkiboss_classic/video/7637590092445404423",
+        },
+        {
+            video: "/Videos/VideoC.mp4",
+            title: "Blind Character Reactions",
+            link: "https://www.tiktok.com/@alkiboss_classic/video/7521858014177955077",
+            preview: "https://www.tiktok.com/@alkiboss_classic/video/752185801417795507",
+        },
+        {
+            video: "/Videos/VideoA.mp4",
+            title: "Cinematic Lifestyle Scenes",
+            link: "https://www.tiktok.com/@alkiboss_classic/video/7405118961689431302",
+            preview: "https://www.tiktok.com/@alkiboss_classic/video/7405118961689431302",
+        },
+        {
+            video: "/Videos/VideoE.mp4",
+            title: "Viral Comedy Moments",
+            link: "https://www.tiktok.com/@alkiboss_classic/video/7521858014177955077",
+            preview: "https://www.tiktok.com/@alkiboss_classic/video/7521858014177955077",
+        }
   ];
-
+   
   interface VideoProps {
     video: string;
     title: string;
