9580374e421f2dd45ac0968aeaa8960f15aaed45
EBUXUI <123589822+EBUXUI@users.noreply.github.com>
2026-06-05 16:05:13 +0300

    Update HeroSection.tsx

--- pages/components/HeroSection.tsx
+++ pages/components/HeroSection.tsx
@@ -202,7 +202,7 @@ export default function HeroSection() {
                   width={520}
                   height={680}
                   itemProp="image"
-                  alt="Alkiboss Classic comedian entertainer and digital content creator"
+                  alt="Alkiboss Classic Kenyan comedian entertainer digital influencer content creator and brand ambassador"
                   className="w-full h-full object-cover object-top"
                   style={{
                     mixBlendMode: "multiply",
